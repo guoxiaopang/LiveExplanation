a64f3d26b8091e083ac433069b3862119be61d44
tiantianlan <200832147@qq.com>
2016-10-25 19:17:39 +0800

    优化

---
+++ GPUImage/GPUImage/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  GPUImage
+//
+//  Created by duoyi on 16/10/25.
+//  Copyright © 2016年 bt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
---
+++ GPUImage/GPUImage/TableViewController.m
@@ -0,0 +1,50 @@
+//
+//  TableViewController.m
+//  GPUImage
+//
+//  Created by duoyi on 16/10/25.
+//  Copyright © 2016年 bt. All rights reserved.
+//
+
+#import "TableViewController.h"
+
+@interface TableViewController ()
+
+@end
+
+@implementation TableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"aa"];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"aa" forIndexPath:indexPath];
+    if (indexPath.row == 0)
+    {
+        cell.textLabel.text = @"GPUImageVideoCamera iOS摄像头实时美颜";
+    }
+    else if (indexPath.row == 1)
+    {
+        cell.textLabel.text = @"GPUImagePicture 静止图像美颜";
+    }
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+@end
