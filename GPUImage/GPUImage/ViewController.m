6b44cb792e53c02b296a03ac33c254bf302f776e
tiantianlan <200832147@qq.com>
2016-10-25 16:58:27 +0800

    添加文件

--- GPUImage/GPUImage/ViewController.m
+++ GPUImage/GPUImage/ViewController.m
@@ -7,23 +7,43 @@
 //
 
 #import "ViewController.h"
+#import "GPUImage.h"
 
 @interface ViewController ()
+
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImage *inputImage = [UIImage imageNamed:@"105"];
+    
+    // 创建过滤器
+    GPUImageBrightnessFilter *filter = [[GPUImageBrightnessFilter alloc] init];
+    filter.brightness = 0.5;
+    [filter forceProcessingAtSize:inputImage.size];
+    [filter useNextFrameForImageCapture]; // 告诉系统从后来捕获过滤器
+    
+    // 处理静止的图像
+    GPUImagePicture *stillPic = [[GPUImagePicture alloc] initWithImage:inputImage];
+    [stillPic addTarget:filter]; //添加过滤器
+    [stillPic processImage]; // 执行渲染
+    
+    UIImage *newImage = [filter imageFromCurrentFramebuffer];
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:newImage];
+    [imageView sizeToFit];
+    [self.view addSubview:imageView];
+    imageView.center = CGPointMake(CGRectGetWidth(self.view.frame)/2, CGRectGetHeight(self.view.frame)/2);
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 
 @end
