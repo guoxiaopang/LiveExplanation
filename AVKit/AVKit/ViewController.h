910e53bda2f952a652371167b5c8e3dbcb95e550
tiantianlan <200832147@qq.com>
2016-10-26 16:52:00 +0800

    添加AVKit

---
+++ AVKit/AVKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVKit
+//
+//  Created by duoyi on 16/10/26.
+//  Copyright © 2016年 bt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AVKit/AVKit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AVKit
+//
+//  Created by duoyi on 16/10/26.
+//  Copyright © 2016年 bt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
