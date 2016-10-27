5383353528fc0df5dd850360c066a0f962c5358a
tiantianlan <200832147@qq.com>
2016-10-27 15:49:17 +0800

    优化

--- AVPlayer/AVPlayer/ViewController.m
+++ AVPlayer/AVPlayer/ViewController.m
@@ -13,6 +13,7 @@
 
 @property (nonatomic, strong) AVPlayer *player;
 @property (nonatomic, strong) AVPlayerItem *item;
+@property (nonatomic, strong) AVAsset *asset;
 
 @end
 
@@ -23,22 +24,40 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor grayColor];
-//  1. 创建视频资源地址URL，可以是网络URL
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"1.mp4" ofType:nil];
-    NSURL *urlStr = [NSURL URLWithString:path];
-//  2. 通过URL创建视频内容对象`AVPlayerItem`,一个视频对应一个`AVPlayerItem`
-    self.item = [AVPlayerItem playerItemWithURL:urlStr];
-//  3. 创建`AVPlayer`视频播放对象，需要一个`AVPlayerItem`进行初始化
-    self.player = [AVPlayer playerWithPlayerItem:self.item];
-    //self.player = [AVPlayer playerWithURL:urlStr];
-//  4. 创建`AVPlayerLayer`播放图层对象，添加到现实视图上去
+
     AVPlayerLayer *layer = [AVPlayerLayer playerLayerWithPlayer:self.player];
-    layer.frame = CGRectMake(0, 0, 200, 300);
-    layer.backgroundColor = [UIColor whiteColor].CGColor;
     [self.view.layer addSublayer:layer];
-    
+    layer.frame = self.view.bounds;
     // 监听播放状态，ReadyToPlay才可以播放
     [self.item addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+}
+
+- (AVAsset *)asset
+{
+    if (!_asset)
+    {
+        NSURL *url = [[NSBundle mainBundle] URLForResource:@"1" withExtension:@"mp4"];
+        _asset = [AVAsset assetWithURL:url];
+    }
+    return _asset;
+}
+
+- (AVPlayerItem *)item
+{
+    if (!_item)
+    {
+        _item = [AVPlayerItem playerItemWithAsset:self.asset];
+    }
+    return _item;
+}
+
+- (AVPlayer *)player
+{
+    if (!_player)
+    {
+        _player = [AVPlayer playerWithPlayerItem:self.item];
+    }
+    return _player;
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context
